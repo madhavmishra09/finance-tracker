05bb9f8ed7fa07b320523d499f33fb7168fe4393
Madhav Mishra <madhavmishra083@gmail.com>
2026-04-20 20:25:37 +0530

    Skeleton of login page has been done

--- src/authentication/login.jsx
+++ src/authentication/login.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+    };
     return (
         <>
             <div>
                 <h1>Welcome Back User!</h1>
             </div>
             <div>
-                <form action="/login">
+                <form onSubmit={handleSubmit} >
                     <label htmlFor="username">User Name</label>
-                    <input type="text" id="username" placeholder="Username" required />
+                    <input type="text" id="username" value={username} onChange={(e)=> setUsername(e.target.value)} placeholder="Username" required />
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" placeholder="Password" required />
-                    <a href="/forgot-password.jsx">Forgot Your Password?</a>
-                    <button>Login</button>
+                    <input type="password" id="password" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Password" required />       
+                    <button type="submit">Login</button>
                 </form>
+                <p><Link to="/forgot-password">Forgot Your Password?</Link></p>
             </div>
         </>
     )
